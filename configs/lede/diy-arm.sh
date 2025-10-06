63e21147887f88fbf05b26c2d1947ce4d07ebd49
ebufang24 <ebufang24@gmail.com>
2025-10-06 20:37:15 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -41,8 +41,6 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 #sed -i 's|^PKG_VERSION.*|PKG_VERSION:=25.8.3|' feeds/small/xray-core/Makefile
 #sed -i 's|^PKG_HASH.*|PKG_HASH:=a7d3785fdd46f1b045b1ef49a2a06e595c327f514b5ee8cd2ae7895813970b2c|' feeds/small/xray-core/Makefile
-#sed -i 's|^PKG_VERSION.*|PKG_VERSION:=1.11.15|' feeds/small/sing-box/Makefile
-#sed -i 's|^PKG_HASH.*|PKG_HASH:=97d58dd873d7cf9b5e4b4aca5516568f3b2e6f5c3dbc93241c82fff5e4a609fd|' feeds/small/sing-box/Makefile
 
 # diy.sh - 固定多个仓库的指定目录到特定 commit
 
