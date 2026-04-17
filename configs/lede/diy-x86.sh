52dd03e948d76ca66312f88bfef4bf64f8cb64b9
ebufang24 <ebufang24@gmail.com>
2026-04-18 07:21:48 +0800

    Update diy-x86.sh

--- configs/lede/diy-x86.sh
+++ configs/lede/diy-x86.sh
@@ -44,19 +44,8 @@ sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/
 #sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 
 # Change default theme
-sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/luci/Makefile
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# ===== iStore 移动到“服务”菜单 =====
-# 找到 store.lua
-STORE_FILE=$(find . -path "*luci-app-store*/luasrc/controller/store.lua")
-if [ -n "$STORE_FILE" ]; then
-  echo "🔧 patching iStore menu..."
-  # 隐藏原入口（不写死31）
-  sed -i 's/entry({"admin", "store"}, call("redirect_index"), _("iStore"), [0-9]*)/local e = entry({"admin", "store"}, call("redirect_index"), _("iStore"), 31)\ne.hidden = true/' $STORE_FILE
-  # 添加服务菜单
-  sed -i '/e.hidden = true/a entry({"admin", "services", "store"}, alias("admin", "store"), _("iStore"), 31)' $STORE_FILE
-fi
+#sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/luci/Makefile
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Add additional packages
 rm -rf feeds/luci/applications/luci-app-mosdns
@@ -68,7 +57,7 @@ rm -rf feeds/small/luci-app-bypass
 rm -rf feeds/small/luci-app-ssr-plus
 #sed -i 's|^PKG_VERSION.*|PKG_VERSION:=25.8.3|' feeds/small/xray-core/Makefile
 #sed -i 's|^PKG_HASH.*|PKG_HASH:=a7d3785fdd46f1b045b1ef49a2a06e595c327f514b5ee8cd2ae7895813970b2c|' feeds/small/xray-core/Makefile
-git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+#git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 sed -i 's|^KERNEL_PATCHVER:=.*|KERNEL_PATCHVER:=6.12|' target/linux/x86/Makefile
 #############################################
 # 🔧 Fix: disable erofs-utils (404 source)
@@ -125,11 +114,11 @@ fetch_repo_dir() {
 #    "package/libs/openssl"
 
 # 固定 kenzo/luci-app-argone-config    
-fetch_repo_dir \
-    "https://github.com/kenzok8/openwrt-packages.git" \
-    "975682568be6782d568901094ae20d25602e4d62" \
-    "luci-app-argone-config" \
-    "feeds/kenzo/luci-app-argone-config"
+#fetch_repo_dir \
+#    "https://github.com/kenzok8/openwrt-packages.git" \
+#    "975682568be6782d568901094ae20d25602e4d62" \
+#    "luci-app-argone-config" \
+#    "feeds/kenzo/luci-app-argone-config"
 
 # 固定 small/luci-app-openclash    
 #fetch_repo_dir \
