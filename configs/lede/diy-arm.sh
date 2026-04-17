c917b2d584f9ec306969b58d4229f4e02e144d5a
ebufang24 <ebufang24@gmail.com>
2026-04-17 14:16:50 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -49,14 +49,12 @@ sed -i 's/services/system/g' feeds/luci/applications/luci-app-cpufreq/luasrc/con
 # Change default theme
 sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/luci/Makefile
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-# ===== iStore 移动到“服务”菜单 =====
 
+# ===== iStore 移动到“服务”菜单 =====
 # 找到 store.lua
 STORE_FILE=$(find . -path "*luci-app-store*/luasrc/controller/store.lua")
-
 # 1. 隐藏原入口
 sed -i 's/entry({"admin", "store"}, call("redirect_index"), _("iStore"), 31)/local e = entry({"admin", "store"}, call("redirect_index"), _("iStore"), 31)\ne.hidden = true/' $STORE_FILE
-
 # 2. 在后面插入“服务菜单入口”
 sed -i '/e.hidden = true/a entry({"admin", "services", "store"}, alias("admin", "store"), _("iStore"), 31)' $STORE_FILE
 
