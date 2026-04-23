0183215aff877033a988430cd5020ee4d57f6fa0
ebufang24 <ebufang24@gmail.com>
2026-04-23 17:30:50 +0800

    Update diy-x86.sh

--- configs/lede/diy-x86.sh
+++ configs/lede/diy-x86.sh
@@ -120,12 +120,12 @@ fetch_repo_dir() {
 #    "luci-app-argone-config" \
 #    "feeds/kenzo/luci-app-argone-config"
 
-# 固定 small/luci-app-openclash    
-#fetch_repo_dir \
-#    "https://github.com/kenzok8/small.git" \
-#    "74f1c2606f0b47b989a1a68a2b4b000123a2f915" \
-#    "luci-app-openclash" \
-#    "feeds/small/luci-app-openclash"
+# 固定 small/sing-box   
+fetch_repo_dir \
+    "https://github.com/kenzok8/small.git" \
+    "13fcc07646aef237e24d2bce413c3b697be46959" \
+    "sing-box" \
+    "feeds/small/sing-box"
     
 # Delete mosdns
 #rm -rf feeds/packages/net/mosdns
