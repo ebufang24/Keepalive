fa4f9208aa3ede9b0c2f2c409b85c86c6374933b
ebufang24 <ebufang24@gmail.com>
2026-08-30 16:40:36 +0800

    Update diy-x86.sh

--- configs/lede/diy-x86.sh
+++ configs/lede/diy-x86.sh
@@ -52,8 +52,8 @@ rm -rf feeds/passwall_packages/geoview
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 rm -rf feeds/small/luci-app-bypass
 rm -rf feeds/small/luci-app-ssr-plus
 #sed -i 's|^PKG_VERSION.*|PKG_VERSION:=25.8.3|' feeds/small/xray-core/Makefile
@@ -147,13 +147,13 @@ sed -i '750a\
                 <tr><td width="33%">&#28304;&#30721;&#58;&#32;&#108;&#101;&#100;&#101;</td><td><a href="https://github.com/coolsnowwolf/lede" style="color: black;" target="_blank">&#28304;&#30721;&#38142;&#25509;</a></td></tr>
 ' package/lean/autocore/files/x86/index.htm
 
-# 修复 dockerd 29.1.1 x86_64 编译时附属程序不存在导致 cp: cannot stat ''
+# 修复 dockerd binary-daemon 缺少附属程序导致 cp: cannot stat ''
 mkdir -p feeds/packages/utils/dockerd/patches
 
 cat > feeds/packages/utils/dockerd/patches/100-fix-binary-daemon.patch <<'EOF'
 --- a/hack/make/binary-daemon
 +++ b/hack/make/binary-daemon
-@@ -13,8 +13,10 @@
+@@ -13,5 +13,7 @@
  	fi
  	echo "Copying nested executables into $dir"
  	for file in containerd containerd-shim-runc-v2 ctr runc docker-init rootlesskit dockerd-rootless.sh dockerd-rootless-setuptool.sh; do
@@ -162,5 +162,5 @@ cat > feeds/packages/utils/dockerd/patches/100-fix-binary-daemon.patch <<'EOF'
 +			cp -f "$(command -v "$file")" "$dir/"
 +		fi
  	done
- 	# vpnkit might not be available for the target platform, see vpnkit stage in
+ }
 EOF
